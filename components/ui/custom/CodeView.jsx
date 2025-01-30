29f6cfb17eb0b38bdac5bd4f18a97e5d966970a4
Virabyan99 <gmparstone99@gmail.com>
2025-01-30 21:13:19 +0400

    Ai functionallity and gemini setup done

---
+++ components/ui/custom/CodeView.jsx
@@ -0,0 +1,10 @@
+
+const CodeView = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CodeView
--- components/ui/custom/Header.jsx
+++ components/ui/custom/Header.jsx
@@ -1,17 +1,22 @@
+"use client"
 import Image from 'next/image'
 
 import Logo from '@/public/logo.svg'
 import { Button } from '../button'
 import Colors from '@/data/Colors'
+import { useContext } from 'react'
+import { UserDetailContext } from '@/context/UserDetailContext'
+
 
 const Header = () => {
+  const { userDetail , setUserDetail } = useContext(UserDetailContext)
   return (
     <div className="p-4 flex justify-between items-center">
       <Image src={Logo} alt="logo" width={40} height={40} />
-      <div className="flex gap-5">
+      { !userDetail?.name && <div className="flex gap-5">
         <Button variant="ghost">Sign In</Button>
         <Button className="text-white" style={{ backgroundColor: Colors.BLUE }}>Get Started</Button>
-      </div>
+      </div>}
     </div>
   )
 }
